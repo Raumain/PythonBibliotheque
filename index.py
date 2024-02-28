ec13e62b6417e2b815bf4df6e172df25c2126b5c
Jocabin <kobyfar@hotmail.com>
2024-02-28 15:54:10 +0100

    rebase

--- index.py
+++ index.py
@@ -5,17 +5,20 @@ DATABASE = 'db/database.db'
 
 app = Flask(__name__)
 
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 @app.route("/")
 def hello_world():
