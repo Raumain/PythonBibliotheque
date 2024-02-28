63b79e1675dfa172d82e9847bc59a7f1b99b2dba
Jocabin <kobyfar@hotmail.com>
2024-02-28 15:53:50 +0100

    rebase

--- index.py
+++ index.py
@@ -20,3 +20,7 @@ def close_connection(exception):
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
