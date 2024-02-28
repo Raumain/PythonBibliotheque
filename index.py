4eaa42e50cad15989478db82016c598ac3e62412
Jocabin <kobyfar@hotmail.com>
2024-02-28 15:53:28 +0100

    rebase

--- index.py
+++ index.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = 'db/database.db'
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-
